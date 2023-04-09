8c4f85cc62a7bf8338eba874846c658602c96d1b
Zeel <zeel.sutariya@dcmail.ca>
2023-04-09 00:44:39 -0400

    final1

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SessionSecret = exports.HostName = exports.RemoteURI = void 0;
+exports.RemoteURI = "mongodb+srv://Zeel:kx9FOQ2PafY2S8P9@webd6201-zeel.xzrfaba.mongodb.net/contacts?retryWrites=true&w=majority";
+exports.HostName = "MongoDB Atlas";
+exports.SessionSecret = "Webd6201SessionSecret";
+//# sourceMappingURL=db.js.map
---
+++ server/config/db.ts
@@ -0,0 +1,6 @@
+// export const LocalURI ="mongodb://127.0.0.1:27017/contacts";
+export const RemoteURI ="mongodb+srv://Zeel:kx9FOQ2PafY2S8P9@webd6201-zeel.xzrfaba.mongodb.net/contacts?retryWrites=true&w=majority";
+// export const HostName="localhost";
+
+export const HostName = "MongoDB Atlas";
+export const SessionSecret = "Webd6201SessionSecret";
