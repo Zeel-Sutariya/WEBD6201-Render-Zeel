fa2375058e6e4fba1e923210560d87652e952ebf
Zeel <zeel.sutariya@dcmail.ca>
2023-08-21 18:16:51 -0600

    privacy policy update

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -20,6 +20,10 @@ router.get('/projects', function(req, res, next) {
   res.render('index', { title: 'Project',page: "projects",displayName:"" });
 });
 
+router.get('/privacypolicy', function(req, res, next) {
+  res.render('index', { title: 'Privacy Policy',page: "privacypolicy",displayName:"" });
+});
+
 
 router.get('/services', function(req, res, next) {
   res.render('index', { title: 'Services',page: "services",displayName:"" });
